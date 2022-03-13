b1e0bfa25c35cbb59b4c793c7561da48990456d3
MaksaDev <jokic.maksim17@gmail.com>
2022-03-13 15:31:29 +0100

    buttoni+func

--- src/components/Calculator/Styled/index.js
+++ src/components/Calculator/Styled/index.js
@@ -40,3 +40,33 @@ export const Current = styled.div`
    
 
 `
+
+export const Button = styled.button`
+   cursor: pointer;
+   font-size: 2rem;
+   border: 1px outset white;
+   outline: none;
+   background-color: rgba(255, 255, 255, 0.75);
+   &:hover {
+    background-color: rgba(255, 255, 255, 0.9);
+   }
+   
+
+   ${ function ( {gridSpan} ){
+
+      if(gridSpan){
+         return `grid-column: span ${gridSpan};`
+      }
+
+
+   }}
+
+   
+   ${({operation}) => operation && `background-color: gray;` } 
+   ${({control}) => control && `background-color: skyblue;` } 
+   ${({equals}) => equals && `border-bottom-right-radius:10px; background-color: gray;` } 
+   ${({period}) => period && `border-bottom-left-radius:10px; background-color: skyblue;` } 
+   
+
+`
+ 
